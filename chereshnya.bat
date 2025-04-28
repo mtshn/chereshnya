58ade671f6b3149f83c07311e3067cff7b2c9d66
mtshn <128024133+mtshn@users.noreply.github.com>
2025-04-28 13:22:43 +0300

    fixed .sh, .bat, pom.xml
    - fixed pom.xml
    - updated bat and sh files (new file name)

--- chereshnya.bat
+++ chereshnya.bat
@@ -1,2 +1,2 @@
 cd chereshnya
-"./svekla/jdk-16.0.2/bin/java.exe" -cp chereshnya-0.0.2-jar-with-dependencies.jar ru.ac.phyche.chereshnya.gui.JavaFXGUIStarter
+"./svekla/jdk-16.0.2/bin/java.exe" -cp chereshnya-0.0.3-alpha2-jar-with-dependencies.jar ru.ac.phyche.chereshnya.gui.JavaFXGUIStarter
--- chereshnya.sh
+++ chereshnya.sh
@@ -1,2 +1,2 @@
 cd chereshnya
-java -cp ./chereshnya-0.0.2-jar-with-dependencies.jar ru.ac.phyche.chereshnya.gui.JavaFXGUIStarter
+java -cp ./chereshnya-0.0.3-alpha2-jar-with-dependencies.jar ru.ac.phyche.chereshnya.gui.JavaFXGUIStarter
